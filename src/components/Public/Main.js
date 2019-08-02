776e072d27080f35faf81175506597a0812df5cc
Matias Garcia <matiasdanielgarcia@gmail.com>
2019-08-02 10:57:44 -0300

    Task 3: Added styles to login view

--- src/components/Public/Main.js
+++ src/components/Public/Main.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-import { withRouter } from 'react-router-dom'
+import {Link, withRouter} from 'react-router-dom'
 import { inject, observer } from 'mobx-react'
 import {
   Image,
   Header,
   Container,
-  Button
+  Button,
+  Grid
 } from 'semantic-ui-react'
 import {
   Form
@@ -28,55 +29,62 @@ export default class Main extends Component {
   }
   render () {
     const { retrieving } = this.state
-    return <Container text>
-      <div>
-        <div>
-          <Image src={require('@/images/img_login_candidate')} style={{height: '210px'}} centered />
-        </div>
-        <div>
-          <Header as='h1'>
-            Candidate Login.
-            <Header.Subheader>
-            Please login here to access your <strong>account</strong>.
-            </Header.Subheader>
-          </Header>
-          <Form
-            name='form'
-            size='large'
-            onValidSubmit={this.formSubmission}
-          >
-            <Form.Input
-              type='email'
-              name='email'
-              label='Email'
-              placeholder='inbox@mail.com'
+    return <Container text className='full-height'>
+      <Grid verticalAlign='middle' stackable columns={2}>
+        <Grid.Row>
+          <Grid.Column>
+            <Image src={require('@/images/img_login_candidate')} style={{height: '210px'}} centered />
+          </Grid.Column>
+          <Grid.Column>
+            <Header as='h1'>
+              <strong>
+                Candidate Login.
+              </strong>
+              <Header.Subheader>
+                Please login here to access your <strong>candidate account</strong>.
+              </Header.Subheader>
+            </Header>
+            <Form
+              name='form'
               size='large'
-              required
-              validations='isEmail'
-              validationErrors={{ isEmail: 'Email is not valid' }}
-              instantValidation
-            />
-            <Form.Input
-              type='password'
-              name='password'
-              label='Password'
-              placeholder='•••••••'
-              size='large'
-              required
-              instantValidation
-            />
-            <Button
-              positive
-              fluid
-              size='large'
-              disabled={retrieving}
-              loading={retrieving}
+              onValidSubmit={this.formSubmission}
             >
-              Login
-            </Button>
-          </Form>
-        </div>
-      </div>
+              <Form.Input
+                type='email'
+                name='email'
+                label='Email'
+                placeholder='inbox@mail.com'
+                size='large'
+                required
+                validations='isEmail'
+                validationErrors={{ isEmail: 'Email is not valid' }}
+                instantValidation
+              />
+              <Form.Input
+                type='password'
+                name='password'
+                label='Password'
+                placeholder='•••••••'
+                size='large'
+                required
+                instantValidation
+              />
+              <div className='link-container'>
+                <Link to='/forgot-your-password'>Forgot your password?</Link>
+              </div>
+              <Button
+                positive
+                fluid
+                size='large'
+                disabled={retrieving}
+                loading={retrieving}
+              >
+                Login
+              </Button>
+            </Form>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </Container>
   }
 }
